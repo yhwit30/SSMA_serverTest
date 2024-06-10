0505651995af8cbe44a392b6175c19d90559fb2e
yhwit30 <yhwit30@gmail.com>
2024-06-10 17:13:15 +0900

    서버 배포 성공한 버전

---
+++ src/main/java/com/ssma/SsmaTestApplication.java
@@ -0,0 +1,13 @@
+package com.ssma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssma/SsmaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
